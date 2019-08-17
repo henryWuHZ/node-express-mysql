40d21594549deaa5d1cb9bfcdb5c232074c22015
Henry_Wu <517700208@qq.com>
2019-08-17 14:32:31 +0800

    非框架配置header

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const path = require('path');
 const config = require('./config/config.js');
 const router = require('./route/router.js');
 
+function setHeader(res) {
+  res.setHeader("Access-Control-Allow-Origin", config.origin);
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
+  res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Max-Age', 1728000);//预请求缓存20天
+}
+
 let server = http.createServer(function (req, res) {
+  setHeader(res);
+  // console.log(res);
   if (req.url.indexOf('/static') > -1) {
     const filePath = path.join(config.root, req.url);
     console.log(req.url, filePath);
@@ -11,15 +21,6 @@ let server = http.createServer(function (req, res) {
   } else if (req.url.indexOf('/api') > -1) {
     console.log('--api', req.url, req.headers, req.body);
   }
-});
-
-server.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", config.origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Max-Age', 1728000);//预请求缓存20天
-  next();
 });
 
 server.listen(config.port, config.hostname, () => {
